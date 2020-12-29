1f53e01568d5d30b831565cae39fe512bac7a3ce
Linton <linton@motorola.com>
2020-12-29 11:19:12 -0400

    test: ensure SignUpController returns an error if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from './signup'
 describe('SignUp Controller', () => {
-  test('Should return 400 if no n ame is provided', () => {
+  test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
@@ -11,5 +11,6 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
