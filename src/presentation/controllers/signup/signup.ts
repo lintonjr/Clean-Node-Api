90af64d364a9893dcbff8c32bef10c1a8ac367e0
Linton <linton@motorola.com>
2021-01-02 12:57:51 -0400

    refactor: move all signup protocols to a single file

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,7 +1,6 @@
-import { HttpResponse, HttpRequest, Controller, EmailValidador } from '../protocols'
-import { MissingParamError, InvalidParamError } from '../errors'
-import { badRequest, serverError } from '../helpers/http-helper'
-import { AddAccount } from '../../domain/usercases/add-account'
+import { HttpResponse, HttpRequest, Controller, EmailValidador, AddAccount } from './signup-protocols'
+import { MissingParamError, InvalidParamError } from '../../errors'
+import { badRequest, serverError } from '../../helpers/http-helper'
 
 export class SignUpController implements Controller {
   private readonly emailValidador: EmailValidador
