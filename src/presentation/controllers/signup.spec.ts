d21ed13ec6be7b41623d23bdbe6b97ddea13cc72
Linton <linton@motorola.com>
2021-01-02 11:22:59 -0400

    test: ensure SignUpController returns 500 if EmailValidador throws

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,6 +2,7 @@ import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { EmailValidador } from '../protocols/email-validator'
+import { ServerError } from '../errors/server-error'
 
 interface SutTypes {
   sut: SignUpController
@@ -108,5 +109,26 @@ describe('SignUp Controller', () => {
     }
     sut.handle(httpRequest)
     expect(isValidSpy).toHaveBeenLastCalledWith('any_email@mail.com')
+  })
+
+  test('Should return 500 if EmailValidador throws', () => {
+    class EmailValidadorStub implements EmailValidador {
+      isValid (email: string): boolean {
+        throw new Error()
+      }
+    }
+    const emailValidadorStub = new EmailValidadorStub()
+    const sut = new SignUpController(emailValidadorStub)
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
