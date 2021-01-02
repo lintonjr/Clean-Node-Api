add2891769f7285f09b6e023a4c8b1f55ad9bd5d
Linton <linton@motorola.com>
2021-01-02 11:13:39 -0400

    test: ensure SignUpController calls EmailValidador with correct email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -94,4 +94,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
+
+  test('Should call EmailValidador with correct email', () => {
+    const { sut, emailValidadorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidadorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenLastCalledWith('any_email@mail.com')
+  })
 })
